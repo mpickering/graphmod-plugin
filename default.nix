f3ea70a9f9dbc3fe6a6340b6b0d0958443943aab
Matthew Pickering <matthewtpickering@gmail.com>
2018-08-08 22:37:06 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,17 +1,23 @@
 let
-  plugin-overlay = import /root/haskell-nix-plugin/overlay.nix ;
-  normal-overlay = import /root/overlay.nix ;
+  # Get the haskell-nix-plugin infrastructure
+  plugin-overlay = (import <nixpkgs> {}).nur.repos.mpickering.overlays.haskell-plugins;
 
-  nixpkgs = import <nixpkgs> { overlays = [normal-overlay plugin-overlay]; };
+  # Use head.hackage to get ghc-8.6.1
+  nixpkgs = import ./nixpkgs.nix { overlays = [plugin-overlay]; };
 
+
+  # Add the unreleased package to the package set
   extension =
     sel: sup: {
-      graphmod-plugin = sel.callCabal2nix "graphmod-plugin" ./graphmod-plugin {};
+      graphmod-plugin = (sel.callCabal2nix "graphmod-plugin" ./graphmod-plugin {});
     };
+  hp = nixpkgs.haskellPackages.extend(extension);
 
-  hp = nixpkgs.ghcHEAD.extend(extension);
+
+
   gv = nixpkgs.graphviz;
 
+  # Define the plugin
   graphmod = { pluginPackage = hp.graphmod-plugin;
                pluginName = "GraphMod";
                pluginOpts = (out-path: ["${out-path}/output"]);
@@ -20,12 +26,13 @@ let
                   ls ${out-path}
                   ls ${out-path}/output
                   graphmod-plugin --indir ${out-path}/output > ${out-path}/out.dot
-                  cat ${out-path}/out.dot | tred | dot -Tpdf > ${out-path}/modules.pdf
+                  cat ${out-path}/out.dot | tred | dot -Gdpi=600 -Tpng > ${out-path}/modules.png
                 ''; } ;
 
 
 in
   with nixpkgs.haskell.lib;
-#  hp.graphmod-plugin
-  (addPlugin graphmod hp.groups).GraphMod
+	# Disable library profiling for GHC bug
+	# Add the plugin to the package we want to build.
+  (addPlugin graphmod (disableLibraryProfiling hp.aeson)).GraphMod
 
